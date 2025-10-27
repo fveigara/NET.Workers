fd2b7822a27acd3cba60e18f44e046077747ce59
Alex <amolinergu@uoc.edu>
2025-10-27 19:46:54 +0100

    Migracion Local de servidor

---
+++ GenteFit/Migrations/202510271822369_Inicial.cs
@@ -0,0 +1,39 @@
+﻿namespace GenteFit.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Inicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Productoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Precio = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Socios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Email = c.String(),
+                        FechaAlta = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Socios");
+            DropTable("dbo.Productoes");
+        }
+    }
+}
---
+++ GenteFit/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace GenteFit.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GenteFit.Data.GenteFitContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(GenteFit.Data.GenteFitContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
